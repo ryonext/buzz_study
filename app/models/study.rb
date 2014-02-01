0410cc9654f8ba23b119cdff67b8ca8ba8dcdf50
ryonext <ryonext.s@gmail.com>
2014-02-01 17:51:35 +0900

    Fix mongo settig again.

--- app/models/study.rb
+++ app/models/study.rb
@@ -1,11 +1,11 @@
 class Study
   include Mongoid::Document
   include Mongoid::Timestamps # adds created_at and updated_at fields
-  store_in collection: "uri"
+  store_in collection: "tweet"
 
   # field <name>, :type => <type>, :default => <value>
-  field :tweet_id, :type => String
-  field :uri, :type => String
+  field :id, :type => String
+  field :original_url, :type => String
 
   # You can define indexes on documents using the index macro:
   # index :field <, :unique => true>
