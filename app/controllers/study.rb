e64af89a46f1f582686bd46f29b466e63487861b
ryonext <ryonext.s@gmail.com>
2014-02-14 23:30:59 +0900

    Use counted list.

--- app/controllers/study.rb
+++ app/controllers/study.rb
@@ -1,23 +1,7 @@
 BuzzStudy::App.controllers :study do
-  
+
   get '/' do
-    @studies = Study.grouped_collection
+    @studies = Study.order_by(:count.desc).limit(20)
     render 'index'
   end
-
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   'Maps to url '/foo/#{params[:id]}''
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
-  
-
 end
--- app/models/study.rb
+++ app/models/study.rb
@@ -1,41 +1,16 @@
 class Study
   include Mongoid::Document
   include Mongoid::Timestamps # adds created_at and updated_at fields
-  store_in collection: "tweet"
+  store_in collection: "uri_map"
 
   # field <name>, :type => <type>, :default => <value>
-  field :id, :type => String
-  field :original_url, :type => String
+  field :uri, :type => String
+  field :title, :type => String
+  field :count, :type => String
 
   # You can define indexes on documents using the index macro:
   # index :field <, :unique => true>
 
   # You can create a composite key in mongoid to replace the default id using the key macro:
   # key :field <, :another_field, :one_more ....>
-
-  class << self
-    def grouped_collection
-      result = Study.collection.aggregate(
-        {
-          "$group" => {
-            "_id" => "$original_url",
-            "count" => {
-              "$sum" => 1
-            }
-          }
-        },
-        {
-          "$sort" => {
-            "count" => -1
-          }
-        }
-      )
-      10.times.map do |i|
-        RankedItem.new(
-          result[i]["_id"], #uri
-          result[i]["count"]
-        )
-      end
-    end
-  end
 end
