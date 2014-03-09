7aa8b5637b41867f1163b1af80c62237a32b348f
ryonext <ryonext.s@gmail.com>
2014-03-09 10:29:52 +0900

    Use map reduced collection.

--- app/controllers/study.rb
+++ app/controllers/study.rb
@@ -1,7 +1,7 @@
 BuzzStudy::App.controllers :study do
 
   get '/' do
-    @studies = Study.order_by(:count.desc).limit(20)
+    @studies = Study.order_by("value.count".desc).limit(10)
     render 'index'
   end
 end
--- app/models/study.rb
+++ app/models/study.rb
@@ -1,12 +1,12 @@
 class Study
   include Mongoid::Document
   include Mongoid::Timestamps # adds created_at and updated_at fields
-  store_in collection: "study_rank"
+  store_in collection: "study_rank2"
 
   # field <name>, :type => <type>, :default => <value>
-  field :uri, :type => String
-  field :title, :type => String
-  field :count, :type => String
+  field "id.uri", :type => String
+  field "value.title", :type => String
+  field "value.count", :type => String
 
   # You can define indexes on documents using the index macro:
   # index :field <, :unique => true>
